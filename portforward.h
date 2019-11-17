9e4de45626ea0c1e98d4626dc6a1593c2f6fea0f
radiactivo <nanowormer@gmail.com>
2019-11-17 11:32:35 +0100

    Init Repo

---
+++ portforward.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h> 
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <signal.h>
+
+#define DIE(msg) perror(msg); exit(1);
+
+void com(int src, int dst);
+int open_forwarding_socket(char *forward_name, int forward_port);
+void accept_connection(int server_socket, char *forward_name, int forward_port);
+int open_listening_port(int server_port);
+void parse_arguments(int argc, char **argv, int *server_port, char **forward_name, int *forward_port);
